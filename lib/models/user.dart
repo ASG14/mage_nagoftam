11ff1bbdd4f30308ff1f9aae6f22a51ba6d3b1d0
ASG14 <asg14.public@gmail.com>
2026-08-18 12:42:59 +0330

    ساخت کلاس کاربر

---
+++ lib/models/user.dart
@@ -0,0 +1,48 @@
+class User
+{
+  final String id;
+  late int _phoneNumber;
+  late String? _firstName;
+  late String? _lastName;
+  final DateTime _registeredAt;
+
+  User({required  this.id, required this._phoneNumber, this._firstName, this._lastName, required this._registeredAt});
+
+  //Setter
+  void setNewPhoneNumber(int newPhoneNumber)
+  {
+    _phoneNumber= newPhoneNumber;
+  }
+  void setNewFirstName(String newFirstname)
+  {
+    _firstName = newFirstname;
+  }
+  void setNewlastName(String newLastName)
+  {
+    _lastName = newLastName;
+  }
+
+  //Getter
+  String getId() 
+  {
+    return id;
+  }
+  int getPhoneNumber()
+  {
+    return _phoneNumber;
+  }
+  String getFirstName()
+  {
+    return _firstName!;
+  }
+  String getLastName()
+  {
+    return _lastName!;
+  }
+  DateTime getRegisteredAt()
+  {
+    return _registeredAt;
+  }
+
+
+}
