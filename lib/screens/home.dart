562c24aa159acaa68dda09b3b5bfa91a5683c80f
ASG14 <asg14.public@gmail.com>
2026-08-20 08:30:18 +0330

    ساخت کلاس گروه کاربران

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,60 +11,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(title: Text('عنوان اپ بار'),backgroundColor: Color.fromARGB(255, 255, 255, 255),),
-          endDrawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: [
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 153, 79, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Scaffold(
+            appBar: AppBar(title: Text('عنوان اپ بار'),backgroundColor: Color.fromARGB(255, 255, 255, 255),),
+            endDrawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 153, 79, 0),
+                    ),
+                    child: Text('هدر کشو'),
                   ),
-                  child: Text('هدر کشو'),
+                  ListTile(
+                    title: Text('گزینه 1'),
+                    onTap: () {
+                      // عمل مورد نظر برای گزینه 1
+                      Navigator.pop(context); // بستن کشو
+                    },
+                  ),
+                  ListTile(
+                    title: Text('گزینه 2'),
+                    onTap: () {
+                      // عمل مورد نظر برای گزینه 2
+                      Navigator.pop(context); // بستن کشو
+                    },
+                  ),
+                ],
+              ),
+            ),
+            body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, mainAxisAlignment: MainAxisAlignment.start,
+              
+              children: [
+                SizedBox(height: 10,),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  color: Colors.white,
+                  child: Row(
+                    
+                    //crossAxisAlignment: CrossAxisAlignment.stretch,
+                    
+                    children: [
+                      Expanded(flex: 1, child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_left))),
+                      Expanded(flex: 8, child: Text('عنوان گروه فعلی', textAlign: TextAlign.center,)),
+                      Expanded(flex: 1, child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_right))),
+                  
+                    ],
+                  ),
                 ),
-                ListTile(
-                  title: Text('گزینه 1'),
-                  onTap: () {
-                    // عمل مورد نظر برای گزینه 1
-                    Navigator.pop(context); // بستن کشو
-                  },
-                ),
-                ListTile(
-                  title: Text('گزینه 2'),
-                  onTap: () {
-                    // عمل مورد نظر برای گزینه 2
-                    Navigator.pop(context); // بستن کشو
-                  },
-                ),
+                Column(),
               ],
+        
+              
             ),
           ),
-          body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, mainAxisAlignment: MainAxisAlignment.start,
-            
-            children: [
-              SizedBox(height: 10,),
-              Container(
-                padding: EdgeInsets.all(8.0),
-                color: Colors.white,
-                child: Row(
-                  
-                  //crossAxisAlignment: CrossAxisAlignment.stretch,
-                  
-                  children: [
-                    Expanded(flex: 1, child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_left))),
-                    Expanded(flex: 8, child: Text('عنوان گروه فعلی', textAlign: TextAlign.center,)),
-                    Expanded(flex: 1, child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_right))),
-                
-                  ],
-                ),
-              ),
-              Column(),
-            ],
-
-            
-          ),
-        ),
+      ),
     );
   }
 }
