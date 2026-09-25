a31935ac3da4e0e2a9242401ee44872e8c5c276e
ASG14 <asg14.public@gmail.com>
2026-09-25 17:37:58 +0330

    Conditional Import

---
+++ lib/services/client_factory.dart
@@ -0,0 +1,8 @@
+import 'client_factory_io.dart'
+    if (dart.library.html) 'client_factory_web.dart';
+
+import 'package:http/http.dart' as http;
+
+http.Client createClient() {
+  return createClientImpl();
+}
